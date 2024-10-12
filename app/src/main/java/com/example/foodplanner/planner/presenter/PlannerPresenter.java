48b731320d6215c7d72a6a13b233a9b626b0f4c3
Ayasharaf13 <ayasharaf444@yahoo.com>
2024-10-12 21:13:40 +0300

    create presenter

--- app/src/main/java/com/example/foodplanner/planner/presenter/PlannerPresenter.java
+++ app/src/main/java/com/example/foodplanner/planner/presenter/PlannerPresenter.java
@@ -1,4 +1,46 @@
 package com.example.foodplanner.planner.presenter;
 
-public class PlannerPresenter {
+
+import android.util.Log;
+
+import com.example.foodplanner.models.RandomMeal;
+import com.example.foodplanner.models.RepositoryInterface;
+import com.example.foodplanner.network.FoodClient;
+import com.example.foodplanner.network.NetworkDelegate;
+import com.example.foodplanner.planner.view.PlannerViewInterface;
+
+import java.util.List;
+
+public class PlannerPresenter implements PlannerPresenterInterface , NetworkDelegate {
+
+
+    RepositoryInterface repo;
+    PlannerViewInterface plannerViewInterface;
+
+
+    public PlannerPresenter (RepositoryInterface repo, PlannerViewInterface plannerViewInterface){
+        this.repo = repo;
+        this.plannerViewInterface = plannerViewInterface;
+    }
+
+    @Override
+    public void OnSuccessResult(List<RandomMeal> listOfMeals) {
+
+        repo.saveMeal(listOfMeals);
+        plannerViewInterface.showMealsPlanner(repo.getAllMealFromStorage());
+
+    }
+
+    @Override
+    public void OnFailureResult(String errorMass) {
+
+        Log.i("failPlanner",errorMass);
+
+    }
+
+    @Override
+    public void getMealsPlanner(String id) {
+
+        repo.getMealByIdNetwork(FoodClient.getInstance().getApiService().getMealsById(id),this);
+    }
 }
