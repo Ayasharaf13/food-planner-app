0addf75370e7c58045e03d29d4f1ba2dc50defe8
Aya Khaled <60974756+Ayasharaf13@users.noreply.github.com>
2024-11-18 19:08:56 +0200

    Update SavePresenterInterface.java

--- app/src/main/java/com/example/foodplanner/save/presenter/SavePresenterInterface.java
+++ app/src/main/java/com/example/foodplanner/save/presenter/SavePresenterInterface.java
@@ -1,15 +1,9 @@
 package com.example.foodplanner.save.presenter;
-
 import androidx.lifecycle.LiveData;
-
 import com.example.foodplanner.models.RandomMeal;
-
 import java.util.List;
 
 public interface SavePresenterInterface {
-
-
-
     public void getMealsByID(String id);
     void delete (List<RandomMeal> meal);
     LiveData<List<RandomMeal>> searchById(String id);
