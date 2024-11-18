bc27d7c707cef50e4804a91e7191e7457ac005ea
Aya Khaled <60974756+Ayasharaf13@users.noreply.github.com>
2024-11-18 19:05:30 +0200

    Update SavePresenterInterface.java

--- app/src/main/java/com/example/foodplanner/save/presenter/SavePresenterInterface.java
+++ app/src/main/java/com/example/foodplanner/save/presenter/SavePresenterInterface.java
@@ -13,20 +13,15 @@ public interface SavePresenterInterface {
     public void getMealsByID(String id);
     void delete (List<RandomMeal> meal);
     LiveData<List<RandomMeal>> searchById(String id);
-<<<<<<< HEAD
-    LiveData<List<RandomMeal>> getAllSavePlannerBySelectedDay(int id);
-=======
-<<<<<<< HEAD
-    LiveData<List<RandomMeal>> getAllSavePlannerBySelectedDay(int id);
-=======
-<<<<<<< HEAD
-    LiveData<List<RandomMeal>> getAllSavePlannerBySelectedDay(int id);
-=======
 
->>>>>>> eca1561c37458cd3dcb6567e08125bd49145daf2
->>>>>>> b48bd45d26781cdb878e9e8b47ee3307893594f3
+    LiveData<List<RandomMeal>> getAllSavePlannerBySelectedDay(int id);
 
->>>>>>> 0d9ef6d7aa17e8c1afc0e5627dc28cab6e65f3b9
+    LiveData<List<RandomMeal>> getAllSavePlannerBySelectedDay(int id);
+
+    LiveData<List<RandomMeal>> getAllSavePlannerBySelectedDay(int id);
+
+
+
 
     LiveData<List<RandomMeal>> getAllSaveBySelectedDay(int id);
 }
